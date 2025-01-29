32d36a014b3027361d37dd6f1e7e462bafd18311
Augusto Pereira <augusto.pereira@unicred.com.br>
2025-01-29 14:47:17 -0300

    Código relacionado ao Desafio 6.

---
+++ Logica_de_Programacao/Desafio_6/desafio_6/Desafio_6.py
@@ -0,0 +1,6 @@
+numero = 0  # Inicia com um valor diferente de 10
+
+while numero != 10:  # Continua pedindo números até que seja 10
+    numero = int(input("Digite um número: "))
+
+print("Número 10 digitado! Programa encerrado.")
