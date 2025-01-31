66b0d45da6234269a3a5a7609d7bb8855f1bce60
Augusto Pereira <augusto.pereira@unicred.com.br>
2025-01-31 12:01:22 -0300

    Parte final do desenvolvimento do desafio 14.

---
+++ Logica_de_Programacao/Desafio_14/desafio_14/Desafio_14.py
@@ -0,0 +1,15 @@
+vetor_nomes = []
+for i in range(20):
+    nome = input(f"Digite o {i + 1}º nome: ")
+    vetor_nomes.append(nome)
+
+vetor_unicos = []
+
+
+for nome in vetor_nomes:
+    if nome not in vetor_unicos:
+        vetor_unicos.append(nome)
+
+print("\nNomes únicos restantes:")
+for nome in vetor_unicos:
+    print(nome)
