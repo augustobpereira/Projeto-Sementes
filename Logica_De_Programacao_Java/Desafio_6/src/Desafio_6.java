3a2aadd11cc266465ca13d0e071f5e0a09003e3c
Augusto Pereira <augusto.pereira@unicred.com.br>
2025-03-12 12:16:50 -0300

    Correção aplicada conforme feedback do mentor do curso.

--- Logica_De_Programacao_Java/Desafio_6/src/Desafio_6.java
+++ Logica_De_Programacao_Java/Desafio_6/src/Desafio_6.java
@@ -8,20 +8,25 @@ public class Desafio_6 {
 
         while (true) {
             System.out.print("Digite um número entre 1 e 10: ");
-            numero = scanner.nextInt();
 
-            if (numero >= 1 && numero <= 10) {
-                System.out.println("Você digitou: " + numero);
+            if (scanner.hasNextInt()) {
+                numero = scanner.nextInt();
 
-                if (numero == 10) {
-                    System.out.println("Programa encerrado.");
-                    break;
+                if (numero >= 1 && numero <= 10) {
+                    System.out.println("Você digitou: " + numero);
+
+                    if (numero == 10) {
+                        System.out.println("Programa encerrado.");
+                        break;
+                    }
+                } else {
+                    System.out.println("ERRO! Digite um número entre 1 e 10.");
                 }
             } else {
-                System.out.println("ERRO! Digite Novamente um número entre 1 e 10.");
+                System.out.println("ERRO! Entrada inválida. Digite um número entre 1 e 10.");
+                scanner.next();
             }
         }
-
         scanner.close();
     }
 }
